c6affff1bfa25fb1fa389eb80c1168761f0d64d4
alsh02 <alsh02@hanyang.ac.kr>
2024-07-24 00:45:31 +0900

    problem 1064 solved

--- 1064.py
+++ 1064.py
@@ -1,13 +1,13 @@
 # 1064번: 평행사변형
 # 1. 세 점이 주어졌으므로 임의의 한 점에서 다른 두 점을 끝점으로 하는 벡터를 고려하자.
 #   1-1. 만약 두 벡터 사이의 각이 180도라면 평행사변형을 만들 수 없다. 바로 -1.0를 출력한다. 종료.
-#   1-2. 두 벡터 사이의 각이 180도가 아닌 경우라면 점 D는 1단계에서 고른 한 점에서 뻗은 각이등분선과 다른 두 점을 이은 대각선의 교점과 점 대칭을 이루는 위치에 존재한다.
-# 2. 1단계에서 점 D를 구한 경우(1-2), 1단계에서 구한 두 벡터의 합의 2배를 계산하여 만든 평행사변형의 둘레를 구한다.
-# 3. 2단계에서 구한 평행사변형의 개수가 2개 이상인 경우 가장 큰 둘레값과 가장 작은 둘레값의 차를 계산하여 출력한다.
-from math import acos, sqrt, degrees
+#   1-2. 두 벡터 사이의 각이 180도가 아닌 경우라면 두 벡터의 합의 2배를 계산하여 만든 평행사변형의 둘레를 구한다.
+# 2. 1단계에서 구한 평행사변형의 개수가 2개 이상인 경우 가장 큰 둘레값과 가장 작은 둘레값의 차를 계산하여 출력한다.
+from math import sqrt
 
 arr = list(map(int, input().split()))
 points = [[arr[i], arr[i + 1]] for i in range(0, len(arr) - 1, 2)]
+rounds = []
 
 for i in range(3):
     start = points[i]
@@ -18,15 +18,19 @@ for i in range(3):
     for k in idx:
         ends.append(points[k])
 
-    vectors = [[start[j] - end_point[j] for j in range(2)] for end_point in ends]
-    
+    vectors = [[end[j] - start[j] for j in range(2)] for end in ends]
+    cross = vectors[0][0] * vectors[1][1] - vectors[0][1] * vectors[1][0]
+    if cross == 0:
+        break
+
     norms = []
     for vector in vectors:
-        norms.append(sqrt(vector[0] ** 2 + vector[1] ** 2))
+        n = sqrt(vector[0] ** 2 + vector[1] ** 2)
+        norms.append(n)
     
-    product_norm = norms[0] * norms[1]
-    dot = vectors[0][0] * vectors[1][0] + vectors[0][1] * vectors[1][1]
+    rounds.append(2 * sum(norms))
 
-    angle = degrees(acos(dot / product_norm))
-    
-    
+if len(rounds) != 3:
+    print(-1.0)
+else:
+    print(max(rounds) - min(rounds))
